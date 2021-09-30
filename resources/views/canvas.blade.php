951d1ee55290b6c18fff99584b8b18d66bad8fa2
zzdaicazz <doandinhhuy2603@gmail.com>
2021-09-30 23:54:15 +0700

    no message

--- resources/views/canvas.blade.php
+++ resources/views/canvas.blade.php
@@ -1,19 +1,34 @@
 <!DOCTYPE html>
 <html>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
 <body>
-
+<div class="container">
 <p>Image to use:</p>
 <img id="hinh1" width="150" height="300" src="{{ asset('assets/images/image1.jpg') }}" alt="The Scream">
 <img id="hinh2" width="150" height="300" src="{{ asset('assets/images/image2.jpg') }}" alt="The Scream">
-
-<p>Canvas:</p>
-<canvas id="canvas" width="500" height="600" style="border:1px solid #d3d3d3; text-align: center;" >
+<div class="row mx-auto p-3">
+	<div class="col-6" style="border:1px solid black;">
+      <p>Canvas:</p>
+			<canvas id="canvas" width="500" height="300" style="text-align: center;" >
+			Your browser does not support the HTML5 canvas tag.
+			</canvas>
+    </div>
+    <div class="col-6" style="border:1px solid black;">
+     <p>GIF:</p>
+			<img id="image" width="500" height="350" />
+    </div>
+</div>
+{{-- <p>Canvas:</p>
+<canvas id="canvas" width="500" height="300" style="text-align: center;" >
 Your browser does not support the HTML5 canvas tag.
 </canvas>
-
-<img id="image" width="500" height="600">
+<p>GIF:</p>
+	<img id="image" width="500" height="600" /> --}}
 
 <button id="download">Download</button>
+</div>
 <script type="text/javascript" src="{{ asset('/js/LZWEncoder.js') }}"></script>
 <script type="text/javascript" src="{{ asset('/js/NeuQuant.js') }}"></script>
 <script type="text/javascript" src="{{ asset('/js/GIFEncoder.js') }}"></script>
@@ -29,14 +44,14 @@ Your browser does not support the HTML5 canvas tag.
 	window.onload = function() {
 		var canvas = document.getElementById("canvas");
 		canvas.width = 500;
-		canvas.height = 600;
+		canvas.height = 350;
 
 		var ctx = canvas.getContext("2d");
 		encoder.setRepeat(0); 
-  		encoder.setDelay(200); 
+  		encoder.setDelay(500); 
   		encoder.start();
   		ctx.fillStyle = "rgb(255,255,255)";  
-		// ctx.fillRect(0,0, canvas.width, canvas.height);
+		ctx.fillRect(0,0, canvas.width, canvas.height);
 		encoder.addFrame(ctx);
 		// encoder.finish();
 		function drawRotatedImage(ctx, image, x, y, width, height, rotation) {
@@ -49,8 +64,6 @@ Your browser does not support the HTML5 canvas tag.
 			ctx.translate(x + halfWidth, y + halfHeight);
 			ctx.rotate(rotation);
 			ctx.drawImage(image, -halfWidth, -halfHeight, width, height);
-
-			// ctx.drawImage(image2, -halfWidth, -halfHeight, width, height);
 			ctx.restore();
 			encoder.addFrame(ctx);
 		}
@@ -62,39 +75,40 @@ Your browser does not support the HTML5 canvas tag.
 				}, 50);
 				return;
 			}
-			// ctx.drawImage(image, 20, 20, 150, 300);
-
-			drawRotatedImage(ctx, image1, 320, 20, 150, 300, Math.PI / 4)
-			drawRotatedImage(ctx, image2, 220, 20, 150, 300, -Math.PI / 4)
-			// encoder.addFrame(ctx);
+			drawRotatedImage(ctx, image1, 220, 20, 150, 300, Math.PI / 4)
+			drawRotatedImage(ctx, image2, 120, 20, 150, 300, -Math.PI / 4)
+			encoder.addFrame(ctx);
 		}
 
 		function animate(image1, imgage2, canvas, context, startTime) {
-	        // update
-	        var time = (new Date()).getTime() - startTime;
-	        var amplitude = 150;
+        // update
+        var time = (new Date()).getTime() - startTime;
+        var amplitude = 150;
 
-	        // in ms
-	        var period = 2000;
-	        var centerX = canvas.width / 2 - image1.width / 2;
-	        var nextX = amplitude * Math.sin(time * 2 * (Math.PI) / period) + centerX;
-	        // image1.x = nextX;
-	        // image2.x = nextX;
+        // in ms
+        var period = 1000;
+        var centerX = canvas.width / 2 - image1.width / 2;
+        var nextX = amplitude * Math.sin(time * 2 * (Math.PI) / period) + centerX;
 
-	        // clear
-	        encoder.start();
-	        ctx.clearRect(0, 0, canvas.width, canvas.height);
-	        // draw
-	        drawRotatedImage(ctx, image1, nextX, 20, 150, 300, Math.PI / 4)
-  			
-			drawRotatedImage(ctx, image2, nextX -100, 20, 150, 300, -Math.PI / 4)
+        console.log(time)
+        console.log(nextX)
+        // image1.x = nextX;
+        // image2.x = nextX;
+
+        // clear
+        encoder.start();
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        // draw
+        drawRotatedImage(ctx, image1, nextX, 20, 150, 300, Math.PI / 4)
 			
-	        // request new frame
-	        requestAnimFrame(function() {
-	          	animate(image1, image2, canvas, ctx, startTime);
-	        });
-	        encoder.finish();
-      	}
+				drawRotatedImage(ctx, image2, nextX -100, 20, 150, 300, -Math.PI / 4)
+		
+        // request new frame
+        requestAnimFrame(function() {
+          	// animate(image1, image2, canvas, ctx, startTime);
+        });
+        encoder.finish();
+    	}
 
 	  	var image1 = document.getElementById("hinh1");
 	  	var image2 = document.getElementById("hinh2");
@@ -103,7 +117,7 @@ Your browser does not support the HTML5 canvas tag.
 
 	  	setTimeout(function() {
 	        var startTime = (new Date()).getTime();
-	        animate(image1, image2, canvas, ctx, startTime);
+	        // animate(image1, image2, canvas, ctx, startTime);
 	  	}, 1000);
 		// encoder.addFrame(ctx);
 	  	encoder.finish();
@@ -123,7 +137,7 @@ Your browser does not support the HTML5 canvas tag.
 		document.getElementById('image').src = data_url
 		// Download
 		
-  		encoder.download("download.gif");
+  		// encoder.download("download.gif");
 
   		var button = document.getElementById("download");
   		document.getElementById("download").addEventListener("click", function() {
